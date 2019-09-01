699cf03811fc70b02ac8b1fd7c5a3d499016d60b
chen188669 <188669@163.com>
2019-09-01 20:45:39 +0800

    friend

--- app/src/main/java/com/chen/fakevibrato/ui/my/adapter/FriendAdapter.kt
+++ app/src/main/java/com/chen/fakevibrato/ui/my/adapter/FriendAdapter.kt
@@ -1,15 +1,19 @@
 package com.chen.fakevibrato.ui.my.adapter
 
 import android.content.Context
+import android.transition.Visibility
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
 import com.bumptech.glide.Glide
 import com.chen.fakevibrato.R
-
+import com.chen.fakevibrato.utils.MyLog
 
 
 /**
@@ -17,12 +21,18 @@ import com.chen.fakevibrato.R
  * @email 188669@163.com
  */
 class FriendAdapter(var mContext : Context, private var mList : List<String>) : RecyclerView.Adapter<FriendAdapter.ViewHolder>() {
+    private var onItemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_friend, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return mList.size
+        return 20
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -30,12 +40,43 @@ class FriendAdapter(var mContext : Context, private var mList : List<String>) : 
                 .load(R.mipmap.logo)
                 .circleCrop()
                 .into(holder.ivHead)
+        holder.ivEdit.setOnClickListener {
+            onItemClickListener?.onRemark(position)
+        }
+        holder.tvMessage.setOnClickListener {
+            onItemClickListener?.onMessage(position)
+        }
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.onItemClick(position)
+        }
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        if (payloads.isEmpty()){
+            onBindViewHolder(holder, position)
+        }else{
+            if (payloads[0] as Boolean){
+                holder.ivEdit.visibility = VISIBLE
+                holder.tvMessage.visibility = GONE
+            }else{
+                holder.tvMessage.visibility = VISIBLE
+                holder.ivEdit.visibility = GONE
+            }
+        }
+
+    }
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
         var ivHead : ImageView = view.findViewById(R.id.ivHead)
         var tvName : TextView = view.findViewById(R.id.tvName)
         var ivEdit : ImageView = view.findViewById(R.id.ivEdit)
         var tvMessage : ImageView = view.findViewById(R.id.tvMessage)
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+        //编辑备注
+        fun onRemark(position: Int)
+        //发消息
+        fun onMessage(position: Int)
     }
 }
--- app/src/main/java/com/chen/fakevibrato/ui/my/view/FriendFragment.kt
+++ app/src/main/java/com/chen/fakevibrato/ui/my/view/FriendFragment.kt
@@ -1,7 +1,11 @@
 package com.chen.fakevibrato.ui.my.view
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.text.InputType
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chen.fakevibrato.R
@@ -9,14 +13,19 @@ import com.chen.fakevibrato.base.BaseFragment
 import com.chen.fakevibrato.ui.home.presenter.MainPresenter
 import com.chen.fakevibrato.ui.my.adapter.FriendAdapter
 import kotlinx.android.synthetic.main.fragment_friend.*
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction
+import android.text.InputType.TYPE_CLASS_TEXT
+
+
 
 /**
  * @author Created by CHEN on 2019/8/29
  * @email 188669@163.com
  * 好友
  */
-class FriendFragment :BaseFragment<MainPresenter>() {
-    var adapter : FriendAdapter? = null
+class FriendFragment : BaseFragment<MainPresenter>() {
+    var adapter: FriendAdapter? = null
 
     var mList = ArrayList<String>()
     override fun setView(): Int {
@@ -28,9 +37,13 @@ class FriendFragment :BaseFragment<MainPresenter>() {
     }
 
     override fun initView(view: View?) {
+        mList.add("哦哦哦")
+        mList.add("哦哦哦")
         adapter = activity?.let { FriendAdapter(it, mList) }
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = adapter
+
+        initListener()
     }
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -39,5 +52,49 @@ class FriendFragment :BaseFragment<MainPresenter>() {
 
     override fun Load() {
 
+    }
+
+    private fun initListener() {
+        tvRemarks.setOnClickListener {
+            tvRemarks.visibility = GONE
+            tvComplete.visibility = VISIBLE
+            adapter?.notifyItemRangeChanged(0, mList.size, true)
+        }
+        tvComplete.setOnClickListener {
+            tvComplete.visibility = GONE
+            tvRemarks.visibility = VISIBLE
+            adapter?.notifyItemRangeChanged(0, mList.size, false)
+        }
+
+        adapter?.setOnItemClickListener(object : FriendAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+
+            }
+
+            override fun onRemark(position: Int) {
+                showEditTextDialog()
+            }
+
+            override fun onMessage(position: Int) {
+
+            }
+        })
+    }
+
+    private fun showEditTextDialog() {
+        val builder = QMUIDialog.EditTextDialogBuilder(activity)
+        builder.setTitle("修改备注名")
+                .setPlaceholder("在此输入备注名")
+                .setInputType(InputType.TYPE_CLASS_TEXT)
+                .addAction("取消") { dialog, index -> dialog.dismiss() }
+                .addAction("确定") { dialog, index ->
+                    val text = builder.editText.text
+                    if (text != null && text.length > 0) {
+
+                        dialog.dismiss()
+                    } else {
+                    }
+                }
+                .create(R.style.dialogTheme).show()
     }
 }
